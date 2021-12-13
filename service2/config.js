b7ed8b8149ee9af4f526f19cd4c0f32b7bc119f3
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-12-13 08:16:08 -0300

    Remoção da chave de acesso mongodb

--- service2/config.js
+++ service2/config.js
@@ -1,3 +1,5 @@
+const { MONGO_DB_KEY } = require('./keys')
+
 module.exports = {
     name: 'service2',
     version: '1.0.0',
@@ -5,6 +7,6 @@ module.exports = {
     port: process.env.PORT || 3000,
     base_url: process.env.BASE_URL || 'http://localhost:3000',
     db: {
-        uri: process.env.MONGODB_URI || 'mongodb+srv://utfpr:utfpr@cluster0.fyase.mongodb.net/restifyDatabase?retryWrites=true&w=majority',
+        uri: process.env.MONGODB_URI || `mongodb+srv://utfpr:${MONGO_DB_KEY}@cluster0.fyase.mongodb.net/restifyDatabase?retryWrites=true&w=majority`,
     },
 };
